ec5f2603e3d9b6499b6c048f9251f6412d009507
Alex <Alex@DESKTOP-JMENRI0>
2023-10-24 21:44:06 -0500

    Finalizado

---
+++ src/pkg4/learn4_gui_ej_cronometro/Learn4_GUI_Ej_Cronometro.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg4.learn4_gui_ej_cronometro;
+
+/**
+ *
+ * @author Alex
+ */
+public class Learn4_GUI_Ej_Cronometro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaCronometro VC= new VentanaCronometro();
+        VC.setVisible(true);
+       
+    }
+    
+}
